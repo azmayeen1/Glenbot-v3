4e7676d9b48bc41a354fb4bf2aaa88efff22db55
azmayeen1 <160739596+azmayeen1@users.noreply.github.com>
2025-05-20 20:17:00 +0600

    Update warn.js

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -8,8 +8,10 @@ module.exports = {
   aliases: [],
   description: "Warn a member. 3 warnings = 1-hour timeout.",
   usage: "warn <@member> [reason]",
-  run: async (client, message, args) => {
-    message.delete();
+//run: async (client, message, args) => {
+  //if (message.deletable) {
+   // message.delete().catch(() => {});
+  }
 
     if (!message.member.permissions.has("MODERATE_MEMBERS"))
       return message.channel.send(`You don't have permission to use this command!`);
